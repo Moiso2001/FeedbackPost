2c3095499e258589ee953950231b7b6e57db5cfc
MoisoDavid <wollxi@gmail.com>
2023-07-05 22:04:59 -0500

    Adding some Food components

---
+++ src/components/CardDetail/Menu/Comments/Comments.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/* DB Types */
+import { TypeComment } from '../../../../db'
+
+type CommentsProps = {
+    comments: Array<TypeComment>
+}
+
+export default function Comments({comments}: CommentsProps) {
+  return (
+    <div>
+
+    </div>
+  )
+}
--- src/components/CardDetail/Menu/Food.tsx
+++ src/components/CardDetail/Menu/Food.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { TypeComment } from '../../../db'
+import Comments from './Comments/Comments'
 
 type FoodProps = {
     img: string
@@ -12,7 +13,28 @@ type FoodProps = {
 export default function Food({img,name,description,comments}: FoodProps ){
     return(
         <div>
-            
+            <div>
+                <img src={img}/>
+            </div>
+            <div>
+                <h3>{name}</h3>
+                <span>{description}</span>
+                <button>REVIEWS</button>
+            </div>  
+            <div>
+                <div>
+                    <span>{comments.length}</span>
+                </div>
+                <div>
+                    {<Comments comments={comments}/>}
+                </div>
+                <div>
+                    <form>
+                        <textarea/>
+                        <button>Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 
