26251bc66918d4c27192951ddcd84abcc9ca4844
MoisoDavid <wollxi@gmail.com>
2023-07-06 23:09:57 -0500

    T

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -6,7 +6,7 @@ import { restaurants, TypeRestaurant } from '../../db'
 
 /* Card import */
 import Card from './Card/Card'
-// Testi
+// Test
 export default function Cards() {
   return (
     <div>
