8d1662d421d279379e318663855ba5f08d690cd9
MoisoDavid <wollxi@gmail.com>
2023-07-08 18:20:41 -0500

    Add Login Website

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,6 +2,39 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+        <div>
+            <img/>
+            <h2>Welcome Back!</h2>
+        </div>
+        <div>
+            <div>
+                <h3>Login</h3>
+                <p>Welcome back! please login to your account.</p>
+            </div>
+            <div>
+                <label>User Name</label>
+                <input type='email'/>
+                <label>Password</label>
+                <input type='password'/>
+            </div>
+            <div>
+                <div>
+                    <input type='checkbox'/>
+                    <label>Remember Me</label>
+                </div>
+                <div>
+                    <span>Forgot password?</span>
+                </div>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+            <div>
+                <span>New User?</span>
+                <span>Singup</span>
+            </div>
+        </div>
+    </div>
   )
 }
