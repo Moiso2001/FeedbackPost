bb404ed3bbf6c8fb4f695b6cb4ea561bfda2ca86
MoisoDavid <wollxi@gmail.com>
2023-07-08 21:50:22 -0500

    Navbar CSS

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,7 @@
 /* React God */
 //import React from 'react';
+/* CSS */
+import s from './Header.module.css'
 
 /* REACT ICONS */
 import {CiSearch} from "react-icons/ci";
@@ -8,27 +10,29 @@ import {MdStars} from "react-icons/md";
 
 export default function Header() {
   return (
-    <div>
-        <div>
-            <h2>Feedback</h2>
-            <h2>Post</h2>
+    <div className={s.div_global}>
+        <div className={s.div_title}>
+            <h1 className={s.title_yellow}>Feedback</h1>
+            <h1 className={s.title_green}>Post</h1>
         </div>
-        <div>
+        <div className={s.div_navbar}>
             <ul>
                 <li>Home</li>
                 <li>Favourites</li>
                 <li>Contact</li>
             </ul>
         </div>
-        <div>
-            <input/>
-            <CiSearch/>
-        </div>
-        <div>
-            <BsPersonCircle/>
-        </div>
-        <div>
-            <MdStars/>
+        <div className={s.div_helpers}>
+            <div className={s.helpers_search}>
+                <input placeholder='Search...'/>
+                <CiSearch className={s.search_icon}/>
+            </div>
+            <div className={s.helpers_icons}>
+                <BsPersonCircle className={s.icon_profile}/>
+            </div>
+            <div className={s.helpers_icons}>
+                <MdStars className={s.icon_premium}/>
+            </div>
         </div>
     </div>
   )
