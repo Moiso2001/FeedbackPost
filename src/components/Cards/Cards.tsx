cc786585b59b78865424efeeb8c5e4f9cdbea6a5
MoisoDavid <wollxi@gmail.com>
2023-07-09 21:43:32 -0500

    Home Page done

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -2,6 +2,8 @@
 import {AiOutlineHeart, AiFillHeart} from "react-icons/ai"
 import {BsFillArrowDownCircleFill} from "react-icons/bs"
 
+import { Link } from "react-router-dom"
+
 /* Import from DB (hardcode) */
 import { restaurants, TypeRestaurant } from '../../db'
 
@@ -16,6 +18,8 @@ export default function Cards() {
   return (
     <div className={s.the_div}>
       <div className={s.div_global}>
+
+      <Link to={'restaurant/1'} style={{textDecoration: 'none'}}>
         <div className={s.div_card1}>
           <div>
               <img src='https://img.theculturetrip.com/wp-content/uploads/2022/01/andres-carne-de-res-restaurant-chia-cundinamarca-colombia.jpg'/>
@@ -28,6 +32,8 @@ export default function Cards() {
               <AiOutlineHeart/> 
           </div>
         </div>
+      </Link>
+      <Link to='restaurant/2' style={{textDecoration: 'none'}}>
         <div className={s.div_card2}>
           <div>
               <img src='https://media-cdn.tripadvisor.com/media/photo-s/15/15/da/15/terraza.jpg'/>
@@ -40,6 +46,8 @@ export default function Cards() {
               <AiFillHeart className={s.icon}/> 
           </div>
         </div>
+      </Link>
+      <Link to='restaurant/3' style={{textDecoration: 'none'}}>
         <div className={s.div_card3}>
           <div>
               <img src='https://d2yoo3qu6vrk5d.cloudfront.net/images/20211113115953/crepes-waffles_-cual-es-el-nuevo-proyecto-que-abrio-fuera-de-bogota_-fotos.jpg'/>
@@ -52,7 +60,9 @@ export default function Cards() {
               <AiOutlineHeart/>   
           </div>
         </div>
+      </Link>
       </div>
+      
       
       <div className={s.div_button}>
         <BsFillArrowDownCircleFill className={s.arrowMore}/>
